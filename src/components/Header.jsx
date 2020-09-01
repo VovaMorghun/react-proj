69551c9353994fa99faf84f1279804e2a901c6d8
vovikm1 <vovikm1@gmai.com>
2020-09-01 17:11:43 +0300

    first template

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+        <img src="https://vistapointe.net/images/logo-4.jpg" alt=""/>
+      </header>
+    )
+}
+
+export default Header
